72342b081f9be44339e8cb66aa6fce9efbe1a1f5
Vedant Vatare <vedantvatare73@gmail.com>
2024-03-16 23:17:40 +0530

    changes in script

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const gameBoard = document.querySelector(".game_board");
-const obstacles = document.querySelector('.obstacles');
-const player = document.querySelector('#player-plane');
-const playArea = document.querySelector('.play_area');
+const obstacles = document.querySelector(".obstacles");
+const player = document.querySelector("#player-plane");
+const playArea = document.querySelector(".play_area");
 const playerWidth = player.clientWidth;
 const playAreaWidth = playArea.clientWidth;
 const playAreaHeight = playArea.clientHeight;
 let targetDuration = 4000;
-document.documentElement.style.setProperty("--target-speed", targetDuration)
-let playerSpeed = 25; 
+document.documentElement.style.setProperty("--target-speed", targetDuration);
+let playerSpeed = 25;
 let movingLeft = false;
 let movingRight = false;
 let movingUp = false;
@@ -15,91 +15,93 @@ let movingDown = false;
 let animationFrameId;
 
 window.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowLeft") {
-        movingLeft = true;
-        console.log("MovingLeft is True")
-    }
-    if (e.key === "ArrowRight") {
-        movingRight = true;
-        console.log("Movingright is True")
-    }
-    if (e.key === "ArrowUp") {
-        movingUp = true;
-        console.log("MovingUp is True")
-    }
-    if (e.key === "ArrowDown") {
-        movingDown = true;
-        console.log("Movingdown is True")
-    }
+  if (e.key === "ArrowLeft") {
+    movingLeft = true;
+  }
+  if (e.key === "ArrowRight") {
+    movingRight = true;
+  }
+  if (e.key === "ArrowUp") {
+    movingUp = true;
+  }
+  if (e.key === "ArrowDown") {
+    movingDown = true;
+  }
 
-    // Start the animation loop only if it's not already running
-    if (!animationFrameId) {
-        animationFrameId = window.requestAnimationFrame(updateGame);
-        console.log("animationFrameId storing. ReqAFrame() for updateGame() @109")
-    }
-});
-
-window.addEventListener("keyup", (e) => {
-    if (e.key === "ArrowLeft") {
-        movingLeft = false;
-    }
-    if (e.key === "ArrowRight") {
-        movingRight = false;
-    }
-    if (e.key === "ArrowUp") {
-        movingUp = false;
-    }
-    if (e.key === "ArrowDown") {
-        movingDown = false;
-    }
-});
-
-window.addEventListener("keyup", (e) => {
-    // Stop the animation loop when no keys are pressed
-    if (!movingLeft && !movingRight && !movingUp && !movingDown) {
-        window.cancelAnimationFrame(animationFrameId);
-        console.log("@160 CancelAnimatnFrame for animationFrameId")
-        animationFrameId = undefined;
-    }
-})
-function updateGame() {
+  // Start the animation loop only if it's not already running
+  if (!animationFrameId) {
     movePlayer();
-    animationFrameId = window.requestAnimationFrame(updateGame);
-}
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  player.dataset.direction = "";
+  if (e.key === "ArrowLeft") {
+    movingLeft = false;
+  }
+  if (e.key === "ArrowRight") {
+    movingRight = false;
+  }
+  if (e.key === "ArrowUp") {
+    movingUp = false;
+  }
+  if (e.key === "ArrowDown") {
+    movingDown = false;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  // Stop the animation loop when no keys are pressed
+  if (!movingLeft && !movingRight && !movingUp && !movingDown) {
+    window.cancelAnimationFrame(animationFrameId);
+    animationFrameId = undefined;
+  }
+});
+
 function movePlayer() {
-    const playerX = parseInt(window.getComputedStyle(player, null).getPropertyValue('left'));
-    const playerY = parseInt(window.getComputedStyle(player, null).getPropertyValue('top'));
+  const playerX = parseInt(
+    window.getComputedStyle(player, null).getPropertyValue("left")
+  );
+  const playerY = parseInt(
+    window.getComputedStyle(player, null).getPropertyValue("top")
+  );
 
-    if (movingLeft && playerX > 60) {
-        player.style.left = playerX - playerSpeed + "px";
-    }
+  if (movingLeft && playerX > 120) {
+    console.log(playerX);
+    player.style.left = playerX - playerSpeed + "px";
+    console.log("moving left")
+    player.dataset.direction = "movingLeft";
+  }
+  
+  if (movingRight && playerX + playerWidth < playAreaWidth + 70) {
+    player.style.left = playerX + playerSpeed + "px";
+    console.log("moving right")
+    player.dataset.direction = "movingRight";
+  }
 
-    if (movingRight && playerX + playerWidth < (playAreaWidth - 20)) {
-        player.style.left = playerX + playerSpeed + "px";
-    }
+  if (movingUp && playerY > 0) {
+    player.style.top = playerY - playerSpeed + "px";
+  }
 
-    if (movingUp && playerY > 0) {
-        player.style.top = playerY - playerSpeed + "px";
-    }
-
-    if (movingDown && playerY + player.clientHeight < playAreaHeight) {
-        player.style.top = playerY + playerSpeed + "px";
-    }
+  if (movingDown && playerY + player.clientHeight < playAreaHeight) {
+    player.style.top = playerY + playerSpeed + "px";
+  }
+  animationFrameId = window.requestAnimationFrame(movePlayer);
 }
-function randomCoords(){
- // targets should be at any left property
+
+function randomCoords() {
+  // targets should be at any left property
 }
-function shootRocket(){
-   let elem = document.createElement('img');
-   elem.setAttribute('src',"rocket.svg");
-   elem.className = "target rocket";
-   obstacles.appendChild(elem);
-   removeTarget(elem);
+function shootRocket() {
+  let elem = document.createElement("img");
+  elem.setAttribute("src", "rocket.svg");
+  elem.className = "target rocket";
+  obstacles.appendChild(elem);
+  removeTarget(elem);
 }
 
-
-function removeTarget(target){
-    setTimeout(()=>{
-        target.remove();
-    }, targetDuration + 500)
+function removeTarget(target) {
+  setTimeout(() => {
+    target.remove();
+  }, targetDuration + 500);
 }
