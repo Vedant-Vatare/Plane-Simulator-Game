8ef5582b49c57eab75138f3da9a66e5bc0be1214
Vedant Vatare <vedantvatare73@gmail.com>
2024-03-25 16:06:23 +0530

    limits of player movements

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const gameBoard = document.querySelector(".game_board");
 const obstacles = document.querySelector(".obstacles");
 const player = document.querySelector("#player-plane");
 const playArea = document.querySelector(".play_area");
-const playerWidth = player.clientWidth;
-const playAreaWidth = playArea.clientWidth;
-const playAreaHeight = playArea.clientHeight;
-let targetDuration = 4000;
-document.documentElement.style.setProperty("--target-speed", targetDuration);
-let playerSpeed = 25;
+// setting player limit fro moving left && right
+const extremeLeft =
+  (document.body.clientWidth - playArea.clientWidth + player.clientWidth) / 2;
+const extremeRight =
+  document.body.clientWidth - extremeLeft - player.clientWidth / 2;
+let targetDuration = 2500;
+document.documentElement.style.setProperty("--target-speed", targetDuration + "ms");
+let playerSpeed = 35;
 let movingLeft = false;
 let movingRight = false;
 let movingUp = false;
 let movingDown = false;
 let animationFrameId;
-
 window.addEventListener("keydown", (e) => {
-  console.log(e.key)
   if (e.key === "ArrowLeft" || e.key === "a") {
     movingLeft = true;
   }
@@ -47,9 +47,6 @@ window.addEventListener("keyup", (e) => {
   if (e.key === "ArrowDown" || e.key === "s") {
     movingDown = false;
   }
-});
-
-window.addEventListener("keyup", (e) => {
   // Stop the animation loop when no keys are pressed
   if (!movingLeft && !movingRight && !movingUp && !movingDown) {
     window.cancelAnimationFrame(animationFrameId);
@@ -65,37 +62,22 @@ function movePlayer() {
     window.getComputedStyle(player, null).getPropertyValue("top")
   );
 
-  if (movingLeft && playerX > 120) {
+  if (movingLeft && playerX > extremeLeft) {
     player.style.left = playerX - playerSpeed + "px";
   }
 
-  if (movingRight && playerX + playerWidth < playAreaWidth + 70) {
+  if (movingRight && playerX < extremeRight) {
     player.style.left = playerX + playerSpeed + "px";
   }
 
   if (movingUp && playerY > 0) {
     player.style.top = playerY - playerSpeed + "px";
   }
-  
-  if (movingDown && playerY + player.clientHeight < playAreaHeight) {
+
+  if (movingDown && playerY + player.clientHeight < playArea.clientHeight) {
     player.style.top = playerY + playerSpeed + "px";
   }
   animationFrameId = window.requestAnimationFrame(movePlayer);
 }
 
-function randomCoords() {
-  // targets should be at any left property
-}
-function shootRocket() {
-  let elem = document.createElement("img");
-  elem.setAttribute("src", "rocket.svg");
-  elem.className = "target rocket";
-  obstacles.appendChild(elem);
-  removeTarget(elem);
-}
 
-function removeTarget(target) {
-  setTimeout(() => {
-    target.remove();
-  }, targetDuration + 500);
-}
